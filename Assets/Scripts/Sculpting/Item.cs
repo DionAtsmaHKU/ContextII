ad1efed401e9888270cb96f43eec77f1fe4f217a
Dion Atsma <dion.atsma@student.hku.nl>
2025-03-16 16:28:34 +0100

    R rotate, F&M flip/mirror

--- Assets/Scripts/Sculpting/Item.cs
+++ Assets/Scripts/Sculpting/Item.cs
@@ -16,6 +16,8 @@ public class Item : MonoBehaviour
     public bool dragging = false;
     private Vector2 offset = Vector2.zero;
     public bool inPlace = false;
+    private float rotateSpeed = -50f;
+    private bool flipped = false;
 
     private void OnEnable()
     {
@@ -28,11 +30,42 @@ public class Item : MonoBehaviour
         // transform.position = new Vector3(transform.position.x, transform.position.y, 0);
         if (!dragging || !toggle.inGame) { return; }
 
+        if (Input.GetKey(KeyCode.R))
+        {
+            transform.Rotate(new Vector3(0, 0, rotateSpeed * Time.deltaTime));
+        }
+
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            FlipVertical();
+        }
+
+        if (Input.GetKeyDown(KeyCode.M))
+        {
+            FlipHorizontal();
+        }
+
         Vector2 mousePos = GetMousePos();
         transform.position = mousePos;
         transform.position = mousePos - offset;
         Debug.Log(mousePos);
         CheckBounds();
+    }
+
+    private void FlipHorizontal()
+    {
+        flipped = !flipped;
+        transform.localScale = new Vector3(transform.localScale.x * -1, transform.localScale.y, transform.localScale.z);
+        if (!flipped) { transform.Rotate(new Vector3(0, 0, 90)); }
+        else { transform.Rotate(new Vector3(0, 0, -90)); }
+    }
+
+    private void FlipVertical()
+    {
+        flipped = !flipped;
+        transform.localScale = new Vector3(transform.localScale.x * -1, transform.localScale.y, transform.localScale.z);
+        if (flipped) { transform.Rotate(new Vector3(0, 0, 90)); }
+        else { transform.Rotate(new Vector3(0, 0, -90)); }
     }
 
     private void OnMouseDown()
